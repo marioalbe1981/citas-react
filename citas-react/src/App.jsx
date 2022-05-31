546918adf09f9ad589500eaa412651b506958c50
marioalbe1981 <73264376+marioalbe1981@users.noreply.github.com>
2022-05-31 14:45:23 -0300

    Add files via upload

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Formularios from './components/Formularios'
+import ListadoPacientes from './components/ListadoPacientes'
+
+
+function App() {
+  
+  const [ pacientes, setPacientes ] = useState([]);
+  const [ paciente, setPaciente ] = useState({});
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+
+      setPacientes(pacientesLS)
+    }
+
+    obtenerLS();
+
+ }, []);
+
+  useEffect(() => {
+     localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes]);
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+
+    setPacientes(pacientesActualizados);
+  }
+   
+  return (
+    <div className="container mx-auto mt-20">
+      <Header 
+        numero={ 1 }
+        isAdmin={ false }
+      />
+      <div className="mt-12 md:flex">
+        <Formularios 
+         pacientes={pacientes} 
+         setPacientes={setPacientes}
+         paciente={paciente}
+         setPaciente={setPaciente}
+        />
+        <ListadoPacientes 
+          pacientes={pacientes} 
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
